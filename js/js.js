8cccb16a5c281b29dd1d4d03235180b19874982b
Carlos Britos <carlosbritos_98@hotmail.com>
2019-04-29 00:08:25 -0300

    sadas

--- js/js.js
+++ js/js.js
@@ -17,179 +17,170 @@ var funciones = {
 
 $(document).ready(function(){
 
-  $('.slideValores').owlCarousel({
-    
-    loop:true,
-    margin:0,
-    nav:false,
-    center:true,
-    dots:false,
-    autoplay:true,
-    items:1,
-
-
-
-    /* responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        800:{
-            items:3
-        }
-    } */
-  })
-
-
-  $('.letras').owlCarousel({
-    
-    loop:true,
-    margin:0,
-    nav:false,
-    center:true,
-    dots:false,
-    autoplay:false,
-    items:30,
-
-
-
-    responsive:{
-        0:{
-            items:10
-        },
-        600:{
-            items:20
-        },
-        800:{
-            items:30
-        }
-    }
-  })
-
-
-  $('.colorClick').on( "click", function(){
-    $('.colorClick').toggleClass("colorNoActive");
-    $(this).toggleClass("colorActive colorNoActive");
-    if($(this).hasClass("comp1")){
-      $(this).toggleClass("col-6 col-12");
-    }
-    else if($(this).hasClass("comp2")){
-      $(this).toggleClass("col-6 col-12");
-    }
-    else{
-      if($(this).hasClass("colorActive")){
-        $('.principal').append(funciones.assambleColorProperties());
+  $('.btn-next').on( "click", function(){
+      if($('.opacityactive').hasClass('first')){
+        $('.first, .last').toggleClass('opacityactive');
       }
       else{
-        $('.colorQ').remove();
-        // $('.principal').remove(funciones.assambleColorProperties());
+        $('.opacityactive').toggleClass('opacityactive').prev().toggleClass('opacityactive');
       }
-    }
-  })
+  
+    })
+  
+    $('.btn-prev').on( "click", function(){
+  
+      if($('.opacityactive').hasClass('last')){
+        $('.first, .last').toggleClass('opacityactive');
+      }
+      else{
+        $('.opacityactive').toggleClass('opacityactive').next().toggleClass('opacityactive');
+      }
+    })
+
+  // $('.responsable-color').on( "click", function(){
+  //   $('.dorado').css('visibility','visible');
+  // })
+
+  // $('.divertido').on( "click", function(){
+  //   $('.rojo').css('visibility','visible');
+  // })
 
 
-  /* SLIDER FOTOS CONSTRUCCION DE LOGO  */
+  // $('.construction').on( "click", function(){
+  //   $('.construction').css('visibility','hidden');
+  // })
+
+  // $('.slideValores').owlCarousel({
+    
+  //   loop:true,
+  //   margin:0,
+  //   nav:false,
+  //   center:true,
+  //   dots:false,
+  //   autoplay:true,
+  //   items:1,
+
+
+
+  //   /* responsive:{
+  //       0:{
+  //           items:1
+  //       },
+  //       600:{
+  //           items:1
+  //       },
+  //       800:{
+  //           items:3
+  //       }
+  //   } */
+  // })
+
+
+  // $('.letras').owlCarousel({
+    
+  //   loop:true,
+  //   margin:0,
+  //   nav:false,
+  //   center:true,
+  //   dots:false,
+  //   autoplay:false,
+  //   items:30,
+
+
+
+  //   responsive:{
+  //       0:{
+  //           items:10
+  //       },
+  //       600:{
+  //           items:20
+  //       },
+  //       800:{
+  //           items:30
+  //       }
+  //   }
+  // })
+
+
+  // $('.colorClick').on( "click", function(){
+  //   $('.colorClick').toggleClass("colorNoActive");
+  //   $(this).toggleClass("colorActive colorNoActive");
+  //   if($(this).hasClass("comp1")){
+  //     $(this).toggleClass("col-6 col-12");
+  //   }
+  //   else if($(this).hasClass("comp2")){
+  //     $(this).toggleClass("col-6 col-12");
+  //   }
+  //   else{
+  //     if($(this).hasClass("colorActive")){
+  //       $('.principal').append(funciones.assambleColorProperties());
+  //     }
+  //     else{
+  //       $('.colorQ').remove();
+  //       // $('.principal').remove(funciones.assambleColorProperties());
+  //     }
+  //   }
+  // })
+
+
+  // /* SLIDER FOTOS CONSTRUCCION DE LOGO  */
 
   
-  $('.btn-next').on( "click", function(){
+  // $('.btn-next').on( "click", function(){
 
-    if($('.opacityactive').hasClass('first')){
-      $('.first, .last').toggleClass('opacityactive');
-    }
-    else{
-      $('.opacityactive').toggleClass('opacityactive').prev().toggleClass('opacityactive');
-    }
+  //   if($('.opacityactive').hasClass('first')){
+  //     $('.first, .last').toggleClass('opacityactive');
+  //   }
+  //   else{
+  //     $('.opacityactive').toggleClass('opacityactive').prev().toggleClass('opacityactive');
+  //   }
 
-  })
+  // })
 
-  $('.btn-prev').on( "click", function(){
+  // $('.btn-prev').on( "click", function(){
 
-    if($('.opacityactive').hasClass('last')){
-      $('.first, .last').toggleClass('opacityactive');
-    }
-    else{
-      $('.opacityactive').toggleClass('opacityactive').next().toggleClass('opacityactive');
-    }
-  })
+  //   if($('.opacityactive').hasClass('last')){
+  //     $('.first, .last').toggleClass('opacityactive');
+  //   }
+  //   else{
+  //     $('.opacityactive').toggleClass('opacityactive').next().toggleClass('opacityactive');
+  //   }
+  // })
 
-  $('.circulo').on("click", function(){
-    if($(this).hasClass('aPrincipal')){
-      $('.colorescent .circulo').css({
-        'width': '80px',
-        'height': '80px',
-      })
-      $(this).css({
-        'width': '90px',
-        'height': '90px',
-      })
-      $('body').css('background','#222');
-    }
-    else if($(this).hasClass('aComp1')){
-      $('.colorescent .circulo').css({
-        'width': '80px',
-        'height': '80px',
-      })
-      $(this).css({
-        'width': '90px',
-        'height': '90px',
-      })
-      $('body').css('background','#ff1b4c');
-    }
-    else{
-      $('.colorescent .circulo').css({
-        'width': '80px',
-        'height': '80px',
-      })
-      $(this).css({
-        'width': '90px',
-        'height': '90px',
-      })
-      $('body').css('background','#fff');
-    }
+  // $('.circulo').on("click", function(){
+  //   if($(this).hasClass('aPrincipal')){
+  //     $('.colorescent .circulo').css({
+  //       'width': '80px',
+  //       'height': '80px',
+  //     })
+  //     $(this).css({
+  //       'width': '90px',
+  //       'height': '90px',
+  //     })
+  //     $('body').css('background','#222');
+  //   }
+  //   else if($(this).hasClass('aComp1')){
+  //     $('.colorescent .circulo').css({
+  //       'width': '80px',
+  //       'height': '80px',
+  //     })
+  //     $(this).css({
+  //       'width': '90px',
+  //       'height': '90px',
+  //     })
+  //     $('body').css('background','#ff1b4c');
+  //   }
+  //   else{
+  //     $('.colorescent .circulo').css({
+  //       'width': '80px',
+  //       'height': '80px',
+  //     })
+  //     $(this).css({
+  //       'width': '90px',
+  //       'height': '90px',
+  //     })
+  //     $('body').css('background','#fff');
+  //   }
 
   })
  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
